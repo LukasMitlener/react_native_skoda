fa7548ee75c146cbbe78aaf4827fdb46379ed2f2
LukasMitlener <97617930+LukasMitlener@users.noreply.github.com>
2023-03-11 20:23:14 +0100

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import { FontAwesome } from '@expo/vector-icons'; 
+import car from "../assets/images/oct.png"
+
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>My Octavia</Text>
+          <Text style={styles.subtitle}>Parked</Text>
+        </View>
+        <FontAwesome name="user-circle" size={30} color="gray" />
+      </View>
+      <Image source={car} style={styles.image} resizeMode="contain" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#161818",
+  },
+  header: {
+    marginTop: 50,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  title: {
+    fontSize: 24,
+    color: "#eee",
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  subtitle: {
+    color: "gray",
+    fontWeight: "500",
+  },
+  image: {
+    width: "100%",
+    height:300
+  }
+});
